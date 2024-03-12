ddf15c15da8af8604a8d75c0e928231790810bcb
ChristianBaciu <christian3110@outlook.it>
2024-03-12 21:55:39 +0100

    Ciao mondo

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $data = [
+        'prova' => 'Ciao Mondo!'
+    ];
+
+    return view('home', compact('data'));
 });
